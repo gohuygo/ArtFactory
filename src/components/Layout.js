1883e28c36d8deb5fd4bfc410e8118a06e9b0ffa
Huy <huy@gohuygo.com>
2018-12-05 13:41:34 -0800

    Installed drizzle

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,8 +6,6 @@ export default(props) => {
     <Fragment>
       <Header builderContract={props.builderContract} accounts={props.accounts}/>
 
-      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
-
       {props.children}
     </Fragment>
   )
